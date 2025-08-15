ba07d0f7b42264536ff1049c8cb7f7634e1ea136
LuizFernando991 <lfernando.r991@gmail.com>
2025-08-15 14:27:27 -0300

    test: add upload-status handler tests

---
+++ services/upload-service/src/tests/upload-status.handler.test.ts
@@ -0,0 +1,37 @@
+import request from 'supertest'
+import express from 'express'
+import { mockRedis } from './mocks/mocks'
+import { getServer } from '@/infra/http/server'
+
+describe('uploadStatusHandler', () => {
+  let app: express.Express
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    app = getServer()
+  })
+
+  it('should return 404 if fileId was not passed', async () => {
+    const res = await request(app).get('/upload-status')
+    expect(res.status).toBe(404)
+  })
+
+  it('should return the sent parts', async () => {
+    mockRedis.get.mockResolvedValueOnce(
+      JSON.stringify({
+        parts: [{ PartNumber: 1, ETag: 'abc' }],
+      }),
+    )
+
+    const res = await request(app).get('/upload-status/test')
+    expect(res.status).toBe(200)
+    expect(res.body.uploadedParts).toEqual([1])
+  })
+
+  it('should return 404 if upload is not found', async () => {
+    mockRedis.get.mockResolvedValueOnce(null)
+    const res = await request(app).get('/upload-status/test')
+    expect(res.status).toBe(404)
+    expect(res.body.message).toBe('Upload not found')
+  })
+})
