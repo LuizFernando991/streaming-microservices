1461957fb64326ce9d4d41601a6e95102fd732f8
LuizFernando991 <lfernando.r991@gmail.com>
2025-08-16 14:16:54 -0300

    feat: add queue adapter

---
+++ services/upload-service/src/infra/adapters/queue/queue.adapter.ts
@@ -0,0 +1,37 @@
+import { connect, Channel, ChannelModel } from 'amqplib'
+import { env } from '@/infra/config/env'
+import { Logger } from '../logger/logger.adapter'
+
+const logger = new Logger('QUEUE_ADAPTER')
+
+export class QueueAdapter {
+  private static instance: QueueAdapter
+  private connection?: ChannelModel
+  private channel?: Channel
+
+  private constructor() {}
+
+  public static async getInstance(): Promise<QueueAdapter> {
+    if (!QueueAdapter.instance) {
+      const adapter = new QueueAdapter()
+      adapter.connection = await connect(env.rabbitmqUrl)
+      adapter.channel = await adapter.connection.createChannel()
+      QueueAdapter.instance = adapter
+      logger.info('[RabbitMQ] Connected.')
+    }
+    return QueueAdapter.instance
+  }
+
+  private async publish(queue: string, message: string): Promise<void> {
+    if (!this.channel) throw new Error('Channel not initialized')
+    await this.channel.assertQueue(queue, { durable: true })
+    this.channel.sendToQueue(queue, Buffer.from(message))
+  }
+
+  async onUploadCompleted(data: {
+    episodeId: string
+    objectkey: string
+  }): Promise<void> {
+    await this.publish(env.uploadedQueueName, JSON.stringify(data))
+  }
+}
