e6cb879412d5639b8059e037278772ad30c4b4d3
LuizFernando991 <lfernando.r991@gmail.com>
2025-08-17 23:50:48 -0300

    feat: add loadenv function

---
+++ services/process-video-service/internal/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	RabbitMQUrl           string `mapstructure:"RABBITMQ_URL"`
+	ProcessedVideoQueue   string `mapstructure:"PROCESSED_VIDEO_QUEUE_NAME"`
+	UploadVideoQueue      string `mapstructure:"UPLOAD_QUEUE_NAME"`
+	FailProcessVideoQueue string `mapstructure:"FAILED_PROCESSED_VIDEO_QUEUE_NAME"`
+	BucketURL             string `mapstructure:"BUCKET_URL"`
+	BucketKey             string `mapstructure:"BUCKET_ACCESS_KEY"`
+	BucketSecret          string `mapstructure:"BUCKET_ACCESS_PASSWORD"`
+	BucketRawName         string `mapstructure:"BUCKET_RAW_NAME"`
+	BucketProcessedName   string `mapstructure:"BUCKET_PROCESSED_NAME"`
+	EnableGPUProcess      bool   `mapstructure:"ENABLE_GPU_PROCESS"`
+	EnableGPUScaleNPP     bool   `mapstructure:"ENABLE_GPU_SCALE_NPP"`
+}
+
+func LoadEnv(path string) (*Config, error) {
+	viper.SetConfigFile(path)
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	viper.SetDefault("ENABLE_GPU_PROCESS", false)
+	viper.SetDefault("ENABLE_GPU_SCALE_NPP", false)
+
+	_ = viper.ReadInConfig()
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
